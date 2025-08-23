8880422450ec53c07708e89a2f16ade544cbaf33
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-08-23 15:40:09 +0530

    BMICalculator2D.java folder to Core-Java branch

--- Array/BMICalculator2D.java
+++ Array/BMICalculator2D.java
@@ -5,26 +5,47 @@ import java.util.Scanner;
 public class BMICalculator2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter number of persons: ");
-        int n = sc.nextInt();
+        int number = sc.nextInt();
 
-        double[][] personData = new double[n][3]; // [][0]=weight, [][1]=height, [][2]=BMI
-        String[] weightStatus = new String[n];
+        double[][] personData = new double[number][3]; 
+        String[] weightStatus = new String[number];
 
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter weight (kg) of person " + (i + 1) + ": ");
-            personData[i][0] = sc.nextDouble();
-            System.out.print("Enter height (m) of person " + (i + 1) + ": ");
-            personData[i][1] = sc.nextDouble();
+        for (int i = 0; i < number; i++) {
+            double weight, height;
+
+            do {
+                System.out.print("Enter weight (kg) of person " + (i + 1) + ": ");
+                weight = sc.nextDouble();
+                if (weight <= 0) {
+                    System.out.println("Weight must be a positive number. Try again.");
+                }
+            } while (weight <= 0);
+
+            do {
+                System.out.print("Enter height (m) of person " + (i + 1) + ": ");
+                height = sc.nextDouble();
+                if (height <= 0) {
+                    System.out.println("Height must be a positive number. Try again.");
+                }
+            } while (height <= 0);
+
+            personData[i][0] = weight;
+            personData[i][1] = height;
         }
 
-        for (int i = 0; i < n; i++) {
-            personData[i][2] = personData[i][0] / (personData[i][1] * personData[i][1]);
-            if (personData[i][2] <= 18.4) {
+        for (int i = 0; i < number; i++) {
+            double weight = personData[i][0];
+            double height = personData[i][1];
+            double bmi = weight / (height * height);
+            personData[i][2] = bmi;
+
+            if (bmi <= 18.4) {
                 weightStatus[i] = "Underweight";
-            } else if (personData[i][2] <= 24.9) {
+            } else if (bmi <= 24.9) {
                 weightStatus[i] = "Normal";
-            } else if (personData[i][2] <= 39.9) {
+            } else if (bmi <= 39.9) {
                 weightStatus[i] = "Overweight";
             } else {
                 weightStatus[i] = "Obese";
@@ -32,13 +53,14 @@ public class BMICalculator2D {
         }
 
         System.out.println("\nResults:");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < number; i++) {
             System.out.println("Person " + (i + 1) +
-                               " | Height: " + personData[i][1] + " m" +
-                               " | Weight: " + personData[i][0] + " kg" +
-                               " | BMI: " + String.format("%.2f", personData[i][2]) +
-                               " | Status: " + weightStatus[i]);
+                    " | Height: " + personData[i][1] + " m" +
+                    " | Weight: " + personData[i][0] + " kg" +
+                    " | BMI: " + String.format("%.2f", personData[i][2]) +
+                    " | Status: " + weightStatus[i]);
         }
+
         sc.close();
     }
 }
