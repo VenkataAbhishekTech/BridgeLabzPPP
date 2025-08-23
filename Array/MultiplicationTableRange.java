0b9c80fa24d00c5a15951817b53f41e93a088e65
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-08-23 13:24:46 +0530

    Add MultiplicationTableRange.java folder to Core-Java branch

---
+++ Array/MultiplicationTableRange.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MultiplicationTableRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number(6-9): ");
+        int num = sc.nextInt();
+
+        int[] multiplicationResult = new int[10];
+
+        if(num<6 || num>9){
+            System.out.println("please enter number between 6 and 9.");
+            return;
+        }
+        for(int i=1;i<=10;i++){
+            multiplicationResult[i-1] = num*i;
+        }
+
+        System.out.println("Multiplication tabel of " + num + "is : ");
+        for(int i = 1;i<=10;i++){
+            System.out.println(num + " * " + i + " = " + multiplicationResult[i-1]);
+        }
+        sc.close();
+    }
+}
