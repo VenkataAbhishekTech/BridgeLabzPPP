32137820998c65605a60df943115fbf317cbf164
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-08-26 18:13:42 +0530

    ArrayIndexOutOfBoundsDemo.java folder to Core-Java branch

---
+++ Strings/ArrayIndexOutOfBoundsDemo.java
@@ -0,0 +1,42 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class ArrayIndexOutOfBoundsDemo {
+    public static void generateException(String[] names) {
+        System.out.println("Accessing index 10: " + names[10]);
+    }
+
+    public static void handleException(String[] names) {
+        try {
+            System.out.println("Accessing index 10: " + names[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException: Invalid index access!");
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] names = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println("\nCalling generateException()...");
+        try {
+            generateException(names);
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e);
+        }
+
+        System.out.println("\nCalling handleException()...");
+        handleException(names);
+        sc.close();
+    }
+}
