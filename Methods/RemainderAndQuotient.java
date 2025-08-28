e20f4bc8107e608b4d24c094635b27ef69df2639
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-08-28 16:01:59 +0530

    RemainderAndQuotient.java folder to Core-Java branch

---
+++ Methods/RemainderAndQuotient.java
@@ -0,0 +1,28 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class RemainderAndQuotient {
+    public static int[] findRemainderAndQuotient(int num,int divisor){
+        int Quotient = num/divisor;
+        int Remainder = num%divisor;
+
+        return new int[]{Remainder,Quotient};
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter the divisor: ");
+        int divisor = sc.nextInt();
+
+        int[] result = findRemainderAndQuotient(num, divisor);
+
+        System.out.println("Quotient = " + result[1]);
+        System.out.println("Remainder = " + result[0]);
+
+        sc.close();
+    }
+}
