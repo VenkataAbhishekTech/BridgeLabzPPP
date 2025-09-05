c2aaf9d2b2ddfe34c41c833577631f7141546423
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-09-05 17:03:01 +0530

    Added StudentReport.java in level2 folder inside JavaClassAndObject

---
+++ JavaClassAndObject/level2/StudentReport.java
@@ -0,0 +1,39 @@
+package JavaClassAndObject.level2;
+
+import java.util.Scanner;
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    public Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+    public void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+    }
+}
+public class StudentReport {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Roll Number: ");
+        int roll = sc.nextInt();
+
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+
+        Student student = new Student(name, roll, marks);
+
+        student.displayDetails();
+
+        sc.close();
+    }
+    
+}
