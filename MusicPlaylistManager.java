bda3cfa0d1348d75b6f39388b5e7437df7506511
VenkataAbhishekTech <abhichintakayala.2005@gmail.com>
2025-09-11 16:24:32 +0530

    Added MusicPlaylistManager.java

---
+++ MusicPlaylistManager.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class MusicPlaylistManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Playlist stored in array
+        String[] songs = {"Song1", "Song2", "Song3"};
+
+        boolean running = true; // while loop condition
+
+        while (running) {
+            // Menu
+            System.out.println("\n--- Music Playlist Manager ---");
+            System.out.println("1. Play all songs");
+            System.out.println("2. Play a song by index");
+            System.out.println("3. Search for a song by name");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1: // Play all songs
+                    System.out.println("\nPlaying all songs:");
+                    for (String song : songs) {
+                        System.out.println("Playing: " + song);
+                    }
+                    break;
+
+                case 2: // Play by index
+                    System.out.print("Enter song index (0 - " + (songs.length - 1) + "): ");
+                    int index = sc.nextInt();
+                    if (index >= 0 && index < songs.length) {
+                        System.out.println("Playing: " + songs[index]);
+                    } else {
+                        System.out.println("Invalid index!");
+                    }
+                    break;
+
+                case 3: // Search by name
+                    System.out.print("Enter song name to search: ");
+                    String name = sc.nextLine();
+                    boolean found = false;
+                    for (String song : songs) {
+                        if (song.equalsIgnoreCase(name)) {
+                            System.out.println("Found and Playing: " + song);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Song not found in playlist.");
+                    }
+                    break;
+
+                case 4: // Exit
+                    System.out.println("Exiting Music Playlist. Goodbye!");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        }
+
+        sc.close();
+    }
+}
